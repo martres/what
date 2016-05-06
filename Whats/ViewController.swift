009bd4d3e0d3eb65416472f5e6a255735823fc31
martres <steven.martreux@epitech.eu>
2016-05-06 09:32:07 -0700

    First commit application Xcode
    First commit

---
+++ Whats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whats
+//
+//  Created by Martreux Steven on 03/05/2016.
+//  Copyright © 2016 me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
